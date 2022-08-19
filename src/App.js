bb65951c744cbd3d8081559ac3437f8bf6e236c7
DidykclickVitaliy <alexeymaininig@gmail.com>
2022-08-19 21:49:13 +0300

    made html structure, added styles,fonts and split html into components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Categories } from "./components/Categories";
+import { Header } from "./components/Header";
+import { Sort } from "./components/Sort";
+import { PizzaBlock } from "./components/PizzaBlock";
+
+import "./scss/app.scss";
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

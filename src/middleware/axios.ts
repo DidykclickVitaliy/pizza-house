2a4bbef74d7fd41f0ef00d2e2a54c05dca2575fb
DidykclickVitaliy <iiceituice@gmail.com>
2023-01-25 20:45:12 +0200

    rename vercel key

--- src/middleware/axios.ts
+++ src/middleware/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_PIZZA_HOUSE
-    ? process.env.REACT_APP_PIZZA_HOUSE
+  baseURL: process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL
     : "https://6304c03394b8c58fd7244553.mockapi.io",
 });
